2f4ae97b8674f8e2e697f6a9455eb6a16019cea0
lihuelmujica <lihuelmujica@gmail.com>
2023-08-04 16:00:24 -0300

    add: GlobalExceptionHandler, ResourceAlreadyExistsException and ResourceNotFoundException

---
+++ src/main/java/com/techforb/unicomer/Exception/ResourceAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.techforb.unicomer.Exception;
+
+public class ResourceAlreadyExistsException extends Exception{
+    public ResourceAlreadyExistsException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/techforb/unicomer/Exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.techforb.unicomer.Exception;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
