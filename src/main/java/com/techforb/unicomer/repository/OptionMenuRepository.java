b4fecbff479b41edf6c35e55aef945dd4b6cb427
lihuelmujica <lihuelmujica@gmail.com>
2023-08-05 23:31:23 -0300

    fix: add @Repository to OptionMenuRepository

--- src/main/java/com/techforb/unicomer/repository/OptionMenuRepository.java
+++ src/main/java/com/techforb/unicomer/repository/OptionMenuRepository.java
@@ -1,6 +1,10 @@
 package com.techforb.unicomer.repository;
 
+import com.techforb.unicomer.model.OptionMenu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface OptionMenuRepository extends JpaRepository<OptionMenuRepository, Integer> {
+@Repository
+public interface OptionMenuRepository extends JpaRepository<OptionMenu, Integer> {
+    boolean existsByNameOrUrl(String name, String url);
 }
